aca79f798664f5f4913333609b8c66492e80fd5f
SalvadorSC98 <salvador.sanchez@alten.es>
2023-10-09 16:36:43 +0200

    5-10 Creación del proyecto

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practica1
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,9 +8,14 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Switch
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var correoRecordado: String? = null
+    private var contrasenaRecordada: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         val texto1 = findViewById<TextInputEditText>(R.id.texto1)
         val texto2 = findViewById<TextInputEditText>(R.id.texto2)
+        val switchRecordar = findViewById<Switch>(R.id.switch1)
 
         val button = findViewById<Button>(R.id.button)
         button.setBackgroundColor(Color.LTGRAY)
@@ -46,9 +53,45 @@ class MainActivity : AppCompatActivity() {
                 actualizarColorBoton(texto1, texto2, button)
             }
         })
-        
 
-        
+
+        // Crear dos objetos Persona
+        val usuario1 = Usuario("Usuario1", "usuario1@example.com", "contrasena1", R.drawable.imagen1)
+        val usuario2 = Usuario("Usuario2", "usuario2@example.com", "contrasena2", R.drawable.imagen2)
+
+        val listaUsuarios = mutableListOf(
+            usuario1,
+            usuario2
+        )
+
+
+        button.setOnClickListener {
+            val email = texto1.text.toString()
+            val contrasena = texto2.text.toString()
+
+            switchRecordar.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    correoRecordado = texto1.text.toString()
+                    contrasenaRecordada = texto2.text.toString()
+                } else {
+                    correoRecordado = null
+                    contrasenaRecordada = null
+                }
+            }
+
+            if (validarCredenciales(email, contrasena, listaUsuarios)) {
+                // Credenciales correctas, mostrar mensaje de éxito
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Credenciales incorrectas, mostrar mensaje de error
+                Toast.makeText(this, "Acceso denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
     }
 
     private fun actualizarColorBoton(texto1: TextInputEditText, texto2: TextInputEditText, button: Button) {
@@ -60,5 +103,16 @@ class MainActivity : AppCompatActivity() {
             button.setBackgroundColor(Color.LTGRAY)
         }
     }
+
+
+    fun validarCredenciales(email: String, contrasena: String, listaUsuarios: List<Usuario>): Boolean {
+        for (usuario in listaUsuarios) {
+            if (usuario.email == email && usuario.contrasena == contrasena) {
+                return true // Credenciales correctas
+            }
+        }
+        return false // Credenciales incorrectas
+    }
+
 
 }
